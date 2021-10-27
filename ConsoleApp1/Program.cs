4674320227c7d38bb56ea9434a947fa589468aca
gangstertoppen <gangstertoppen@gmail.com>
2021-10-27 21:51:34 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int temp;
+        int count = 0;
+        int[] num = { 89, 76, 45, 92, 67, 12, 99 };
+        for (int i = 1; i < (num.Length - 1); i++)
+        {
+            for (int j = 0; j < (num.Length - 1); j++)
+            {
+                count++; //teller antall runder.
+                if (num[j + 1] < num[j])
+                {
+                    //Counter 
+                    temp = num[j + 1];
+                    num[j + 1] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Rounds: " + count + "and" + num.LongLength);
+        Console.WriteLine(String.Join(", ", num));
+    }
+}
